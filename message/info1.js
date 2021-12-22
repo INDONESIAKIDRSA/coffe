ca0ca6ff3ad5673a8bd4324e605a3ee7c8dd9cb8
feedzBOT <95519752+feedzBOT@users.noreply.github.com>
2021-12-22 20:38:19 +0800

    Update info1.js

--- message/info1.js
+++ message/info1.js
@@ -1,25 +1,25 @@
 exports.bot = (gender,fake,nomerOwner)=> {
 return`
-*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧𝗭 」
+*╭─▸*「 𝗜𝗡𝗙𝗢 𝗕𝗢𝗧 」
 *│*
 *│▸* Nama : ${fake}
 *│▸* Gender : ${gender}
 *│▸* Hoby : -
 *│▸* Owner : ${nomerOwner}
-*│▸* Tgl pembuatan : 25-07-2021
-*│▸* Tgl release : 29-09-2021
+*│▸* Tgl pembuatan : -
+*│▸* Tgl release : -
 *│▸* Image : Gawr Gura
 *│▸* Voice : Nina kawai
 *│*
 *│▸* SCRIPT BY
-*│*   Decode Denpa
+*│*   Dcode Denpa
 *│*   X-Dev Team
 *│*   Yogi PW
-*│*   Hexagon
+*│*   Hexagonz
 *│*   Dttaz
 *│*
 *│▸* FEATURE  BY  
-*│*   Decode Denpa
+*│*   Dcode Denpa
 *│*   Fernazer
 *│*   X-Dev Team
 *│*   Resta Gamteng
